9f1891f182df32ee84959afba1067ead70c35b03
Valik Grinevich <valik.v1per@gmail.com>
2014-12-20 23:14:36 +0200

    add pagination on front page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,12 +1,12 @@
 <?php
-
 namespace AppBundle\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use AppBundle\Entity;
-
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -15,9 +15,19 @@ class DefaultController extends Controller
      * @Route("/")
      * @Method({"GET"})
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $posts = $this->getDoctrine()->getManager()->getRepository('AppBundle:Post')->findAll();
-        return ['posts' => $posts];
+
+        $paginator  = $this->get('knp_paginator');
+        $posts = $paginator->paginate(
+            $posts,
+            $request->query->get('page', 1),
+            5
+        );
+
+        return [
+            'posts' => $posts
+        ];
     }
 }
