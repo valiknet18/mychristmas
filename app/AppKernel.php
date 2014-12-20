b42a8a40eaf26b1bb0ce4b8f9c50bff6e4efdca3
Valik Grinevich <valik.v1per@gmail.com>
2014-12-20 09:15:45 +0200

    installed additional required bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Hautelook\AliceBundle\HautelookAliceBundle(),
+            new Mremi\UrlShortenerBundle\MremiUrlShortenerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
