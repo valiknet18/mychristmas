e9d3986bbf0b4bc6900081cead204e582a1bafc9
jackjeka <angie.or.jackie@gmail.com>
2014-12-20 19:10:31 +0200

    fix view

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -12,6 +12,6 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Mychristmas")')->count() > 0);
     }
 }
