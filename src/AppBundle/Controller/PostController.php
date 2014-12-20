6cfb4962d578e7ea3babf80118bd20db09a94bfb
Valik Grinevich <valik.v1per@gmail.com>
2014-12-20 14:45:28 +0200

    create action top posts

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -1,13 +1,30 @@
 <?php
-
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template as Template;
 
+/**
+ * @Route("/post")
+ */
 class PostController extends Controller
 {
-    public function addAction()
+    /**
+     * @Route("/top/{type}", defaults= {"type" = "like"}, requirements={"type" = "like|dislike"})
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function topAction($type)
     {
+        $posts = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('AppBundle:Post');
+
+        return [
+            "posts" => $posts,
+            "type" => $type
+        ];
     }
 }
